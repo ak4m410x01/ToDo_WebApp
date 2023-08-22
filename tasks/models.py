947f6d3d955740f36a97362fbd6e2fac70f1542f
ak4m410x01 <abdallah10kamal@gmail.com>
2023-08-23 01:29:28 +0300

    Implement add new tasks

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=500)
+    slug = models.SlugField(unique=True, editable=False)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now=True)
+    modified = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Task, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .models import Task
+from .forms import TaskFrom
 
 # Create your views here.
+
+
+def index(request):
+    tasks = Task.objects.all()
+
+    if request.method == "POST":
+        form = TaskFrom(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+    else:
+        form = TaskFrom()
+
+    context = {
+        "tasks": tasks,
+        "form": form,
+    }
+    return render(request, "list.html", context)
